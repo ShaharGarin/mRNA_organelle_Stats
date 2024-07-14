f5bacf0383f21c0fae3d49692a6bc853df1b450f
ShaharGarin <121550005+ShaharGarin@users.noreply.github.com>
2024-07-14 14:52:18 +0300

    some gui work

--- mRNA_organelle_colo.py
+++ mRNA_organelle_colo.py
@@ -6,6 +6,9 @@ from tkinter import filedialog
 import os
 import pandas as pd
 import tkinter as tk
+from tkinter import ttk
+from ttkthemes import ThemedTk
+
 
 total_mrna = "Total mRNA per Cell"
 col_ner_title = "Total Colocolized With Organelle Near"
@@ -19,6 +22,15 @@ cer_col_rat = "cER Colocalization Ration"
 not_col_rat = "Not Colocalized Ratio"
 ava_org_cov = "Average Organelle Coverage"
 title_order = [total_mrna, tot_col_rat, not_col_rat, ner_col_rat, cer_col_rat]
+
+
+#Browse folder command funciton
+def get_dir():
+    sel_dir = filedialog.askdirectory(initialdir = '', title = "Where are your files?", mustexist = True)
+    tables_folder_path.set(sel_dir)
+
+#Create gui window
+gui = ThemedTk(theme = 'yaru')
 tables_folder_path = tk.StringVar()
 
 #Browse folder command funciton
@@ -26,43 +38,47 @@ def get_dir():
     sel_dir = filedialog.askdirectory(initialdir = '', title = "Where are your files?", mustexist = True)
     tables_folder_path.set(sel_dir)
 
-#create gui window
-gui = tk.Tk()
+#Design gui window
 gui.title("mRNA-Organelle Colocalization Tool")
 gui.geometry('1280x720')
-folder_lbl = tk.Button(gui, text = "Browse Folder", command = get_dir)
+folder_lbl = ttk.Button(gui, text = "Browse Folder", command = get_dir)
 folder_lbl.grid(row = 1, column = 0)
-folder_entry = tk.Entry(gui, textvariable = tables_folder_path)
+folder_entry = ttk.Entry(gui, textvariable = tables_folder_path)
 folder_entry.grid(row = 2, column = 0)
 #get mRNA intesities min/max
-mrna_int = tk.Label(gui, text = 'Enter mRNA Intesities values:')
+mrna_int = ttk.Label(gui, text = 'Enter mRNA Intesity values:')
 mrna_int.grid(row = 3, column = 0)
-mrna_min_lbl = tk.Label(gui, text = "min:")
+mrna_min_lbl = ttk.Label(gui, text = "min:")
 mrna_min_lbl.grid(row = 4, column = 0)
-mrna_min = tk.Entry(gui)
+mrna_min = ttk.Entry(gui)
 mrna_min.grid(row = 4, column = 1)
-mrna_max_lbl = tk.Label(gui, text = "max:")
+mrna_max_lbl = ttk.Label(gui, text = "max:")
 mrna_max_lbl.grid(row = 5, column = 0)
-mrna_max = tk.Entry(gui)
+mrna_max = ttk.Entry(gui)
 mrna_max.grid(row = 5, column = 1)
 #get organelle coverages min/max
-org_int = tk.Label(gui, text = 'Enter Organelle Coverage values:')
+org_int = ttk.Label(gui, text = 'Enter Organelle Coverage values:')
 org_int.grid(row = 6, column = 0)
-org_min_lbl = tk.Label(gui, text = "min:")
+org_min_lbl = ttk.Label(gui, text = "min:")
 org_min_lbl.grid(row = 7, column = 0)
-org_min = tk.Entry(gui)
+org_min = ttk.Entry(gui)
 org_min.grid(row = 7, column = 1)
-org_max_lbl = tk.Label(gui, text = "max:")
+org_max_lbl = ttk.Label(gui, text = "max:")
 org_max_lbl.grid(row = 8, column = 0)
-org_max = tk.Entry(gui)
+org_max = ttk.Entry(gui)
 org_max.grid(row = 8, column = 1)
 #Run filtering and analysis
 
 #Plot filtered data
 
 
+
+
+
+
 def main():
-    my_strain_dic = calc_tables(folder_lbl.)
+ 
+    my_strain_dic = calc_tables(tables_folder_path)
     #statistics_table(tables_folder_path)
     statistics_table(tables_folder_path + new_folder_string, my_strain_dic)
     
@@ -135,4 +151,5 @@ def get_sample_names():
     return strains_dict
 
 
-main()
+gui.mainloop()
+#main()
